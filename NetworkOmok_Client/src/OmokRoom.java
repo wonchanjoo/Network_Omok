afb6ca37442c5354affd117ed9349ebdb40cd614
skc4234 <skc4234@naver.com>
2022-11-23 16:19:14 +0900

    OmokRoom 생성자

--- NetworkOmok_Client/src/OmokRoom.java
+++ NetworkOmok_Client/src/OmokRoom.java
@@ -6,4 +6,9 @@ public class OmokRoom {
 	public int currentPeoples = 0;
 	public String[] player = new String[2];
 	public Vector<String> viewer = new Vector<>();
+	
+	public OmokRoom(int roomId, String[] player) {
+		this.roomId = roomId;
+		this.player = player;
+	}
 }
