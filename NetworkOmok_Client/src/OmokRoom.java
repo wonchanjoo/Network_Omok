49ebf1f258039f8a0ab8adf09fb605ff71e95187
chanjoo <wcj0911@naver.com>
2022-11-23 16:08:53 +0900

    OmokRoom 추가

---
+++ NetworkOmok_Client/src/OmokRoom.java
@@ -0,0 +1,9 @@
+import java.util.Vector;
+
+public class OmokRoom {
+	public String roomName;
+	public int roomId;
+	public int currentPeoples = 0;
+	public String[] player = new String[2];
+	public Vector<String> viewer = new Vector<>();
+}
